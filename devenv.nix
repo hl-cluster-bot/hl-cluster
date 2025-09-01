1841e991bfadb1c3a40489b27458bcb23b84c896
Pavel Popov <me@popov.wtf>
2025-09-01 12:36:53 +0800

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -34,28 +34,12 @@
     alejandra.enable = true;
     ansible-lint.enable = true;
     check-yaml.enable = true;
+    deadnix.enable = true;
+    shellcheck.enable = true;
+    statix.enable = true;
     end-of-file-fixer.enable = true;
-    gptcommit.enable = true;
     shfmt.enable = true;
     trufflehog.enable = true;
     yamllint.enable = true;
   };
-
-  overlays = [
-    # Temporary workaround for https://github.com/NixOS/nixpkgs/issues/400373
-    (final: prev: rec {
-      python312 = prev.python312.override {
-        packageOverrides = final: prev: {
-          mocket = prev.mocket.overridePythonAttrs (oldAttrs: rec {
-            disabledTests =
-              oldAttrs.disabledTests
-              ++ [
-                "test_httprettish_httpx_session"
-              ];
-          });
-        };
-      };
-      python312Packages = python312.pkgs;
-    })
-  ];
 }
